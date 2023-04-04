ef97c6d91ffcec0db68d589635331c9ae8bb7bb9
Roman <rom.badrutdinov2012@yandex.ru>
2023-04-04 12:47:00 +0300

    Задали функции

--- DZ_IT/Program.cs
+++ DZ_IT/Program.cs
@@ -7,3 +7,24 @@
 // [“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
 string[] array1 = { "Hello", "2", "world", ":-)" };
 string[] array2 = new string[array1.Length];
+void SecondArrayWithIF(string[] array1, string[] array2)
+{
+  int count = 0;
+  for (int i = 0; i < array1.Length; i++)
+  {
+    if (array1[i].Length <= 3)
+    {
+      array2[count] = array1[i];
+      count++;
+    }
+  }
+}
+
+void PrintArray(string[] array)
+{
+  for (int i = 0; i < array.Length; i++)
+  {
+    Console.Write($"{array[i]} ");
+  }
+  Console.WriteLine();
+}
